df0a1b82c21e129f1efed790c118169d0a51c182
duruoxian <drx00112233@qq.com>
2026-03-22 16:06:13 +0800

    文件太混乱了，从新提交

---
+++ GoToCellDialog/build/C_2026_03_20_ed21f5-Debug/qtcsettings.cmake
@@ -0,0 +1,13 @@
+# This file is managed by Qt Creator, do not edit!
+
+set("CMAKE_PREFIX_PATH" "/opt/Qt/6.10.2/gcc_64" CACHE "PATH" "" FORCE)
+set("CMAKE_CXX_COMPILER" "/usr/bin/g++" CACHE "FILEPATH" "" FORCE)
+set("CMAKE_BUILD_TYPE" "Debug" CACHE "STRING" "" FORCE)
+set("CMAKE_CXX_FLAGS_INIT" "-DQT_QML_DEBUG" CACHE "STRING" "" FORCE)
+set("CMAKE_C_COMPILER" "/usr/bin/gcc" CACHE "FILEPATH" "" FORCE)
+set("QT_QMAKE_EXECUTABLE" "/opt/Qt/6.10.2/gcc_64/bin/qmake" CACHE "FILEPATH" "" FORCE)
+set("CMAKE_COLOR_DIAGNOSTICS" "ON" CACHE "BOOL" "" FORCE)
+set("CMAKE_VERBOSE_MAKEFILE" "ON" CACHE "BOOL" "" FORCE)
+set("CMAKE_PROJECT_INCLUDE_BEFORE" "/run/media/root/Windows/study/code/group_7_store/Qt-Software-Group7/GoToCellDialog/build/C_2026_03_20_ed21f5-Debug/.qtc/package-manager/auto-setup.cmake" CACHE "FILEPATH" "" FORCE)
+set("QT_MAINTENANCE_TOOL" "/opt/Qt/MaintenanceTool" CACHE "FILEPATH" "" FORCE)
+set("CMAKE_GENERATOR" "Ninja" CACHE "STRING" "" FORCE)
